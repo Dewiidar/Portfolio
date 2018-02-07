4cfce799bfbee2f66602ce5d88c75d81719f153f
Mohammed Dewidar <dewiidar@yahoo.com>
2018-02-07 13:02:04 +0200

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
+
+var jsSources = [
+  'components/scripts/template.js'
+];
 
 gulp.task('log', function(){
   gutil.log("Mohamed Dewidar's Portfolio")
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/js'))
 });
